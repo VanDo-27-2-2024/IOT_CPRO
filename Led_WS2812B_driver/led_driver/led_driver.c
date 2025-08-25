cb449a4e325448987b3cc23791766a3672a10de4
Okai <vandooxx@gmail.com>
2025-08-26 02:22:36 +0700

    Led driver

---
+++ Led_WS2812B_driver/led_driver/led_driver.c
@@ -0,0 +1,67 @@
+#include "led_driver.h"
+
+static led_strip_t *led_strip_p;
+
+int led_init(size_t num_pixels)
+{
+    if (num_pixels == 0)
+    {
+        printf("Invalid pixels size\n");
+        return -1;
+    }
+
+    // Free old buffer if already initialized to avoid memory leak
+    if (led_strip_p != NULL)
+    {
+        free(led_strip_p->color_strip);
+        // led_strip_p->color_strip = NULL; // unnecessary -> cuz the struct is gone when free led_strip_p
+        free(led_strip_p);
+        led_strip_p = NULL;
+    }
+
+    led_strip_p = malloc(sizeof(led_strip_t));
+    if (led_strip_p == NULL)
+    {
+        return -2; // Allocation failed
+    }
+
+    led_strip_p->color_strip = calloc(num_pixels, sizeof(color_data_structure_t));
+    if (led_strip_p->color_strip == NULL)
+    {
+        free(led_strip_p);
+        led_strip_p = NULL;
+
+        return -2; // Allocation failed
+    }
+
+    led_strip_p->led_length = num_pixels;
+
+    return 0;
+}
+
+
+void led_shutdown()
+{
+    if (led_strip_p != NULL)
+    {
+        free(led_strip_p->color_strip);
+        free(led_strip_p);
+        led_strip_p = NULL;
+    }
+}
+
+
+void led_set_pixel_color(size_t index, uint8_t r, uint8_t g, uint8_t b)
+{
+    if (!led_strip_p || index >= led_strip_p->led_length)
+    {
+        printf("Invalid index\n");
+        return;
+    }
+
+    uint32_t color = ((uint32_t)g << 16) | ((uint32_t)r << 8) | (uint32_t)b; // Format: 0x00GGRRBB
+    (led_strip_p->color_strip + index)->color = color;
+}
+
+
+
---
+++ Led_WS2812B_driver/led_driver/led_driver.h
@@ -0,0 +1,67 @@
+#ifndef LED_DRIVER_H
+#define LED_DRIVER_H
+
+#include <stdlib.h>
+#include <stdint.h>
+
+
+typedef struct color_data_structure
+{
+    uint32_t color;
+} color_data_structure_t;
+
+
+typedef struct led_strip
+{
+    color_data_structure_t *color_strip;
+    uint16_t led_length;
+} led_strip_t;
+
+
+/*
+ * led_init - Initializes a buffer for the LED strip with the given number of pixels
+ * @num_pixels:	Number of pixels want to control
+ *
+ * Returns 0 on success, -1 on failure (e.g., memory allocation failed)
+ */
+int led_init(size_t num_pixels);
+
+
+/*
+Frees the memory allocated for the LED strip
+*/
+void led_shutdown();
+
+
+/*
+Sets the color of a specific pixel at the given 'index'
+Uses 3 separate 8-bit values for Red, Green, and Blue
+*/
+void led_set_pixel_color(size_t index, uint8_t r, uint8_t g, uint8_t b);
+
+
+/*
+Fills the entire strip with a single color
+*/
+void led_fill(uint8_t r, uint8_t g, uint8_t b);
+
+
+/*
+Turns off all pixels (by setting their color to black)
+*/
+void led_clear();
+
+
+/*
+Gets a constant (read-only) pointer to the data buffer, ready to be "sent" out
+This function is used for testing purposes in this exercise
+*/
+const uint32_t* led_get_buffer();
+
+
+/*
+Gets the number of pixels on the LED strip
+*/
+size_t led_get_pixel_count();
+
+#endif
